330ad7108caae235ec2971e7ef7d2fa8078cfead
Funcer8 <Funcer@outlook.com>
2021-11-27 19:58:44 +0800

    Refactor, use native method to update warnings real-time

--- resources/MoeUpload.js
+++ resources/MoeUpload.js
@@ -1,72 +1,82 @@
-$(function() {
-	var wpUploadDescription = $("#wpUploadDescription");
-	if (wpUploadDescription.length > 0) {
-		var toggleLink = $("<a/>");
-		toggleLink.attr("href", "javascript:void(0);").text(mw.msg("moeupload-ForManagementOnly")).on("click", function() {
-			wpUploadDescription.slideToggle();
-			return false;
-		});
-		wpUploadDescription.hide();
-		$('.mw-htmlform-field-HTMLTextAreaField .mw-input').prepend(toggleLink);
-	}
-	var errorP = $("<p/>").addClass("error");
-	var uploadFormMsgRow = $("<tr/>").addClass("error uploadFormMsg");
-	var uploadFormMsgCol = $("<td/>").attr("colspan", "2");
-	uploadFormMsgRow.append(uploadFormMsgCol);
-	/*url输入验证*/
-	var wpSrcUrl = $('#wpSrcUrl');
-	var upLoadFileUrlmsg = uploadFormMsgRow.clone().removeClass("uploadFormMsg").hide();
-	wpSrcUrl.closest("tr").after(upLoadFileUrlmsg);
-	wpSrcUrl.on("change blur", function() {
-		var str = wpSrcUrl.val().trim();
-		const supported = mw.config.get('wgFileExtensions').join('|');
-		if (new RegExp('\.(?:' + supported + ')$', 'i').test(str)) {
-			upLoadFileUrlmsg.show().find("td").text(mw.msg("moeupload-PageInsteadOfImg"));
-		} else if ($("#wpUploadFileURL").val() === str) {
-			upLoadFileUrlmsg.show().find("td").text(mw.msg("moeupload-SameAsSourceURL"));
-		} else {
-			upLoadFileUrlmsg.hide();
+mw.hook( 'htmlform.enhance' ).add( function ( $root ) {
+	var srcUrlChecker, categoryChecker;
+
+	function checkUrl( url ) {
+		var d = $.Deferred(), ret = { valid: true, messages: [] };
+		if ( url !== '' ) {
+			const supported = mw.config.get( 'wgFileExtensions' ).join( '|' );
+			if ( new RegExp( '\.(?:' + supported + ')$', 'i' ).test( url ) ) {
+				ret = { valid: false, messages: [ mw.msg( 'moeupload-PageInsteadOfImg' ) ] };
+			} else if ( $root.find(  '#wpUploadFileURL:enabled' ).val() === url ) {
+				ret = { valid: false, messages: [ mw.msg( 'moeupload-SameAsSourceURL' ) ] };
+			}
 		}
-	});
+		d.resolve( ret );
+		return d.promise();
+	}
+	srcUrlChecker = new mw.htmlform.Checker( $root.find( '#wpSrcUrl' ), checkUrl );
+	srcUrlChecker.attach();
+
+	const regexp_title = new RegExp( '[^' + mw.config.get( 'wgLegalTitleChars' ) + ']|' +
+		mw.message( 'moeupload-symbols-regexp' ).plain() );
+	function checkCategory( category ) {
+		var d = $.Deferred(), ret = { valid: true, messages: [] };
+		if ( category !== '' ) {
+			if ( regexp_title.test( category ) ) {
+				ret = { valid: false, messages: [ mw.msg( 'moeupload-HaveSymbol' ) ] };
+			}
+		}
+		d.resolve( ret );
+		return d.promise();
+	}
+	categoryChecker = new mw.htmlform.Checker( $root.find( '#wpCharName' ), checkCategory );
+	categoryChecker.attach();
+	categoryChecker = new mw.htmlform.Checker( $root.find( '#wpAuthor' ), checkCategory );
+	categoryChecker.attach();
+} );
+
+$( function() {
+	var descBox = $( '#wpUploadDescription' );
+	if ( descBox.length > 0 ) {
+		var toggleLink = $( '<a/>' ).attr( 'href', 'javascript:void(0);' );
+		toggleLink.text( mw.msg( 'moeupload-ForManagementOnly' ) ).on( 'click', function() {
+			descBox.slideToggle();
+			return false;
+		} );
+		descBox.hide();
+		descBox.before( toggleLink );
+	}
+	var fieldBox = $( '#mw-htmlform-description' );
+	fieldBox.find( 'tr:last-child' ).after( '<tr><td class="moeupload-warning" colspan=2></td></tr>' );
+	var warningBox = fieldBox.find( '.moeupload-warning' );
 	/* XpAhH同学写的上传页面检测，未写注释禁止上传 */
-	var uploadForm = $("#mw-upload-form");
-	uploadForm.on("submit", function() {
-		uploadForm.find(".inputError").removeClass("inputError");
-		uploadForm.find(".uploadFormMsg").remove();
-		if (mw.config.get( 'wgMoeUploadSkipWarning' ) || mw.util.getParamValue("disableUploadCheck") === "true") {
+	$( '#mw-upload-form' ).on( 'submit', function() {
+		var ifHaveFile = $( '#wpUploadFile:enabled, #wpUploadFileURL:enabled' ).val() !== '';
+		if ( !ifHaveFile ) {
+			warningBox.addClass( 'warningbox' ).text( mw.msg( 'moeupload-NoFile' ) );
+			$( '#wpUploadFileURL, #wpUploadFile' ).one( 'change', function() {
+				warningBox.removeClass( 'warningbox' ).text( '' );
+			} );
+			return false;
+		}
+		if (mw.config.get( 'wgMoeUploadSkipWarning' ) || mw.util.getParamValue( 'disableUploadCheck' ) === 'true' ) {
 			return true;
 		}
-		var returnValue = true;
-		var ifHaveFile = $($('input[name="wpSourceType"]:checked').val() == "url" ? "#wpUploadFileURL" : "#wpUploadFile").val() !== "";
-		if (!ifHaveFile) {
-			$("#mw-htmlform-source").parent().before(errorP.clone().addClass("uploadFormMsg").text(mw.msg("moeupload-NoFile")));
-			returnValue = false;
-		}
 		//三选一
-		var haveNoDetail = $("#wpCharName, #wpAuthor, #wpSrcUrl").filter(function() { return $(this).val().length === 0; });
-		if (haveNoDetail.length === 3) {
-			haveNoDetail.addClass("inputError");
-			var noDetailRow = uploadFormMsgRow.clone();
-			noDetailRow.find("td").text(mw.msg("moeupload-NoDetail"));
-			haveNoDetail.first().closest("tr").before(noDetailRow);
-			returnValue = false;
-		}
-		//符号
-		const regexp = new RegExp(mw.msg("moeupload-symbols-regexp"));
-		var haveSymbol = $("#wpCharName, #wpAuthor, #wpSrcUrl").filter(function() {
-			return regexp.test($(this).val());
+		var haveNoDetail = fieldBox.find( '#wpCharName, #wpAuthor, #wpSrcUrl' ).filter( function () {
+			return this.innerText.length === 0;
 		});
-		if (haveSymbol.length > 0) {
-			haveSymbol.addClass("inputError");
-			var haveSymbolRow = uploadFormMsgRow.clone();
-			haveSymbolRow.find("td").text(mw.msg("moeupload-HaveSymbol"));
-			haveSymbol.first().closest("tr").before(haveSymbolRow);
-			returnValue = false;
+		if ( haveNoDetail.length === 3 ) {
+			$( '#wpDestFile-warning' ).addClass( 'warningbox' ).text( mw.msg( 'moeupload-NoDetail' ) );
+			$( '#wpCharName, #wpAuthor, #wpSrcUrl' ).one( 'change', function() {
+				warningBox.removeClass( 'warningbox' ).text( '' );
+			} );
+			return false;
 		}
-		/*url提交验证*/
-		if (upLoadFileUrlmsg.is(":visible")) {
-			returnValue = false;
+		// Errors populated by mw.htmlform.Checker
+		if ( fieldBox.find( '.errorbox' ).length > 0 ) {
+			return false;
 		}
-		return returnValue;
-	});
-});
+		return true;
+	} );
+} );
